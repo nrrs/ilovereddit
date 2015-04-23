a3f596a4fe40b265461a22b89397531cf85e9e88
nrrs <norris.kwan@gmail.com>
2015-04-22 22:53:36 -0500

    add and style details and features.

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function generateOutput() {
 		var subName = subs[i].toString(),
 			url = link + subName,
 			subJSON = link + subName + "/new.json?t=hour&limit=3";
-		output = output + "<div id='"+subName+"' class='subreddit'><header><strong>" + subName + "</strong><a href='"+url+"' title='"+subName+"' target='_blank' class='url'>("+url+")</a></header><ul></ul></div> <hr/>";
+		output = output + "<div id='"+subName+"' class='subreddit'><header><strong>" + subName + "</strong><a href='"+url+"' title='"+subName+"' target='_blank' class='subreddit-url'>("+url+")</a></header><ul></ul></div>";
 		$('#output').html(output);
 		$.ajax({
 			type: "GET",
@@ -28,11 +28,21 @@ function generateOutput() {
 				for (i = 0; i < 3; i++) {
 					var author = goods.data.children[i].data.author,
 						title = goods.data.children[i].data.title,
+						isNSFW = goods.data.children[i].data.over_18,
+						nsfw = "",
 						url = goods.data.children[i].data.url,
 						permalink = goods.data.children[i].data.permalink,
+						ups = goods.data.children[i].data.ups,
+						downs = goods.data.children[i].data.downs,
+						score = goods.data.children[i].data.score,
 						itemOutput = "";
-					itemOutput = "<li><a href='"+url+"' target='_blank'>"+title+"</a></li>";
+					if(isNSFW) {
+						var nsfw = "over-18";
+					}
+					itemOutput = "<li><a href='"+url+"' target='_blank' class='post-link'>"+title+"</a><span class='author'><strong>Posted By: </strong>"+author+"</span><a href='"+permalink+"' title='permalink' target='_blank' class='permalink'>permalink</a><span class='score'>ups: "+ups+", downs: "+downs+", score: "+score+"<span class='"+nsfw+"'></span></li>";
+
 					$('#' + subName + ' ul').append(itemOutput);
+					
 				}
 								
 			}
